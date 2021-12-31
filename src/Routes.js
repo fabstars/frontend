7c430b9b02c965c4507fdc4716e07e666a5a2294
Navneet-Singh-123 <snavneet561@gmail.com>
2021-12-31 18:47:57 +0530

    Fixing navbar and search options

--- src/Routes.js
+++ src/Routes.js
@@ -24,7 +24,6 @@ const Routes = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={LandingPage} />
-        <Route path="/home" exact component={Home} />
         <Route path="/shop" exact component={Shop} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
