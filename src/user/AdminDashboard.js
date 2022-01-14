a3e3159d571e599917cbfae1304b051ac4980fb7
Navneet-Singh-123 <snavneet561@gmail.com>
2022-01-14 22:25:44 +0530

    Social links

--- src/user/AdminDashboard.js
+++ src/user/AdminDashboard.js
@@ -33,6 +33,11 @@ const AdminDashboard = () => {
               Manage Products
             </Link>
           </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to={`/profile/${_id}`}>
+              Update Profile
+            </Link>
+          </li>
         </ul>
       </div>
     );
--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,93 +1,226 @@
-import React, { useState, useEffect } from 'react';
-import Layout from '../core/Layout';
-import { isAuthenticated } from '../auth';
-import { Link, Redirect } from 'react-router-dom';
-import { read, update, updateUser } from './apiUser';
+import React, { useState, useEffect, Fragment } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link, Redirect } from "react-router-dom";
+import { read, update, updateUser } from "./apiUser";
 
 const Profile = ({ match }) => {
-    const [values, setValues] = useState({
-        name: '',
-        email: '',
-        password: '',
-        error: false,
-        success: false
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: false,
+    success: false,
+    twitter: "",
+    facebook: "",
+    linkedin: "",
+    youtube: "",
+    instagram: "",
+    role: "",
+  });
+
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  const { token } = isAuthenticated();
+  const {
+    name,
+    email,
+    password,
+    error,
+    success,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram,
+    role,
+  } = values;
+
+  const init = (userId) => {
+    // console.log(userId);
+    read(userId, token).then((data) => {
+      console.log(data);
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({
+          ...values,
+          name: data.name,
+          email: data.email,
+          twitter: !data.social ? "" : data.social.twitter,
+          facebook: !data.social ? "" : data.social.facebook,
+          linkedin: !data.social ? "" : data.social.linkedin,
+          youtube: !data.social ? "" : data.social.youtube,
+          instagram: !data.social ? "" : data.social.instagram,
+          role: data.role,
+        });
+      }
     });
+  };
 
-    const { token } = isAuthenticated();
-    const { name, email, password, error, success } = values;
+  useEffect(() => {
+    init(match.params.userId);
+  }, []);
 
-    const init = userId => {
-        // console.log(userId);
-        read(userId, token).then(data => {
-            if (data.error) {
-                setValues({ ...values, error: true });
-            } else {
-                setValues({ ...values, name: data.name, email: data.email });
-            }
+  const handleChange = (name) => (e) => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    update(match.params.userId, token, {
+      name,
+      email,
+      password,
+      twitter,
+      facebook,
+      linkedin,
+      youtube,
+      instagram,
+    }).then((data) => {
+      if (data.error) {
+        // console.log(data.error);
+        alert(data.error);
+      } else {
+        updateUser(data, () => {
+          setValues({
+            ...values,
+            name: data.name,
+            email: data.email,
+            success: true,
+          });
         });
-    };
+      }
+    });
+  };
 
-    useEffect(() => {
-        init(match.params.userId);
-    }, []);
+  const redirectUser = (success) => {
+    if (success) {
+      if (role === "0") {
+        return <Redirect to="/admin/dashboard" />;
+      } else {
+        return <Redirect to="/user/dashboard" />;
+      }
+    }
+  };
 
-    const handleChange = name => e => {
-        setValues({ ...values, error: false, [name]: e.target.value });
-    };
+  const profileUpdate = (name, email, password) => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          onChange={handleChange("name")}
+          className="form-control"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          type="email"
+          onChange={handleChange("email")}
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          type="password"
+          onChange={handleChange("password")}
+          className="form-control"
+          value={password}
+        />
+      </div>
 
-    const clickSubmit = e => {
-        e.preventDefault();
-        update(match.params.userId, token, { name, email, password }).then(data => {
-            if (data.error) {
-                // console.log(data.error);
-                alert(data.error);
-            } else {
-                updateUser(data, () => {
-                    setValues({
-                        ...values,
-                        name: data.name,
-                        email: data.email,
-                        success: true
-                    });
-                });
-            }
-        });
-    };
+      <div className="my-2">
+        <button
+          onClick={() => toggleSocialInputs(!displaySocialInputs)}
+          type="button"
+          className="btn btn-light"
+        >
+          Add Social Network Links
+        </button>
+        <span>Optional</span>
+      </div>
 
-    const redirectUser = success => {
-        if (success) {
-            return <Redirect to="/cart" />;
-        }
-    };
+      {displaySocialInputs && (
+        <Fragment>
+          <div className="form-group social-input">
+            <i className="fab fa-twitter fa-2x"></i>
+            <input
+              type="text"
+              placeholder="Twitter URL"
+              name="twitter"
+              value={twitter}
+              onChange={handleChange("twitter")}
+            />
+          </div>
 
-    const profileUpdate = (name, email, password) => (
-        <form>
-            <div className="form-group">
-                <label className="text-muted">Name</label>
-                <input type="text" onChange={handleChange('name')} className="form-control" value={name} />
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Email</label>
-                <input type="email" onChange={handleChange('email')} className="form-control" value={email} />
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Password</label>
-                <input type="password" onChange={handleChange('password')} className="form-control" value={password} />
-            </div>
+          <div className="form-group social-input">
+            <i className="fab fa-facebook fa-2x"></i>
+            <input
+              type="text"
+              placeholder="Facebook URL"
+              name="facebook"
+              value={facebook}
+              onChange={handleChange("facebook")}
+            />
+          </div>
 
-            <button onClick={clickSubmit} className="btn btn-primary">
-                Submit
-            </button>
-        </form>
-    );
+          <div className="form-group social-input">
+            <i className="fab fa-youtube fa-2x"></i>
+            <input
+              type="text"
+              placeholder="YouTube URL"
+              name="youtube"
+              value={youtube}
+              onChange={handleChange("youtube")}
+            />
+          </div>
 
-    return (
-        <Layout title="Profile" description="Update your profile" className="container-fluid">
-            <h2 className="mb-4">Profile update</h2>
-            {profileUpdate(name, email, password)}
-            {redirectUser(success)}
-        </Layout>
-    );
+          <div className="form-group social-input">
+            <i className="fab fa-linkedin fa-2x"></i>
+            <input
+              type="text"
+              placeholder="Linkedin URL"
+              name="linkedin"
+              value={linkedin}
+              onChange={handleChange("linkedin")}
+            />
+          </div>
+
+          <div className="form-group social-input">
+            <i className="fab fa-instagram fa-2x"></i>
+            <input
+              type="text"
+              placeholder="Instagram URL"
+              name="instagram"
+              value={instagram}
+              onChange={handleChange("instagram")}
+            />
+          </div>
+        </Fragment>
+      )}
+
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  return (
+    <Layout
+      title="Profile"
+      description="Update your profile"
+      className="container-fluid"
+    >
+      <h2 className="mb-4">Profile update</h2>
+      {profileUpdate(name, email, password)}
+      {redirectUser(success)}
+    </Layout>
+  );
 };
 
 export default Profile;
