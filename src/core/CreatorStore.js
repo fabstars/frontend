b39b9ec6eaaa2f5fc4417946406007dd48b36016
Ayush yadav <yayush2002@gmail.com>
2022-01-21 20:55:44 +0530

    creator store fixed

--- src/core/CreatorStore.js
+++ src/core/CreatorStore.js
@@ -7,18 +7,18 @@ const CreatorStore = ({ match }) => {
   return (
     <>
     <div className="bgcolor">
+      <hr className="divider1" />
       <div className="main-banner-creator">
-        <hr className="divider1" />
         <div className="store-heading">
-          <h1>Hritik Roshan</h1>
+          <h1>BluePine Labs</h1>
         </div>
         <div className="creator-social-profiles adjust-height">
-          <a href="#"><i className="fab fa-facebook"></i></a>
-          <a href="#"><i className="fab fa-instagram"></i></a>
-          <a href="#"><i className="fab fa-twitter"></i></a>
-          <a href="#"><i className="fab fa-youtube"></i></a>
-          <a href="#"><i className="fab fa-discord"></i></a>
-          <a href="#"><i className="fab fa-reddit"></i></a>
+          <a href="#" style={{"color": "#3b5998"}}><i className="fab fa-facebook"></i></a>
+          <a href="#" style={{"color": "#e95950"}}><i className="fab fa-instagram"></i></a>
+          <a href="#" style={{"color": "#00acee"}}><i className="fab fa-twitter"></i></a>
+          <a href="#" style={{"color": "#FF0000"}}><i className="fab fa-youtube"></i></a>
+          <a href="#" style={{"color": "#5663F7"}}><i className="fab fa-discord"></i></a>
+          <a href="#" style={{"color": "#FF4500"}}><i className="fab fa-reddit"></i></a>
         </div>
         <HighlightedLinks />
       </div>
--- src/core/components/CreatorStore/HighlightedLinks.js
+++ src/core/components/CreatorStore/HighlightedLinks.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const HighlightedLinks = () => {
   return (
-    <div className="row mt-5">
-      <div className="col-md-12">
+    <div className="mt-5">
+      <div className="">
         <div className="text-center mt-3">
-          <button class="button-64"><span class="text">Highlighted Links</span></button>
+          <a  href=" " class="button-24">Watch my collaboration with this NGO</a>
         </div>
       </div>
-      <div className="col-md-12">
-        <div className="btn-wrapper text-center" style={{"margin-top": "90px"}}>
-          <button class="button-64"><span class="text">Highlighted Links</span></button>
+      <div className="">
+        <div className="text-center" style={{"margin-top": "10px"}}>
+          <button class="button-24">Watch my latest video on moj</button>
         </div>
       </div>
       {/* <div className="col-md-12">
