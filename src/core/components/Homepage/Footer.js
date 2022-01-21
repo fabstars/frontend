419dd498eae32697c1d80646f22d0b7c4d43c1c1
Navneet-Singh-123 <snavneet561@gmail.com>
2022-01-22 00:08:32 +0530

    Fix broken links

--- src/core/components/Homepage/Footer.js
+++ src/core/components/Homepage/Footer.js
@@ -4,23 +4,42 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="footer u-container">
-      <p className="footer-logo" style={{"fontSize": "15px"}}>
+      <p className="footer-logo" style={{ fontSize: "15px" }}>
         fab &copy; 2022 All rights reserved.
       </p>
       <div className="footer__links">
-        <Link className="footer__link" href="" to="/PrivacyPolicy" target="_top" style={{"fontSize": "20px"}}>
+        <Link
+          className="footer__link"
+          href=""
+          to="/PrivacyPolicy"
+          style={{ fontSize: "20px" }}
+        >
           Privacy
         </Link>
-        <Link className="footer__link" href="" to="/ReturnPolicy" target="_top" style={{"fontSize": "20px"}}>
+        <Link
+          className="footer__link"
+          href=""
+          to="/ReturnPolicy"
+          style={{ fontSize: "20px" }}
+        >
           Return Policy
         </Link>
-        <Link className="footer__link" href="" to="/Terms" target="_top"  style={{"fontSize": "20px"}}>
-        Terms of Service
+        <Link
+          className="footer__link"
+          href=""
+          to="/Terms"
+          style={{ fontSize: "20px" }}
+        >
+          Terms of Service
         </Link>
-        <Link className="footer__link" to="/about" target="_top" style={{ fontSize: "20px" }}>
+        <Link className="footer__link" to="/about" style={{ fontSize: "20px" }}>
           About us
         </Link>
-        <Link className="footer__link" to="/contact" target="_top"  style={{ fontSize: "20px" }} >
+        <Link
+          className="footer__link"
+          to="/contact"
+          style={{ fontSize: "20px" }}
+        >
           Contact us
         </Link>
       </div>
